9f4f072a2dafd8f128aba791e830af64b8476806
AlexandraaLupu <alexlupu973@gmail.com>
2024-03-25 14:56:11 +0200

    Modified tests and pom.xml

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -22,7 +22,10 @@ public class ServiceTest {
     private StudentValidator studentValidator;
     private Service service;
 
-    @BeforeAll
+    public ServiceTest() {
+        createXML();
+    }
+
     static void createXML() {
         File xml = new File("fisiere/studentiTest.xml");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(xml))) {
@@ -37,7 +40,6 @@ public class ServiceTest {
         }
     }
 
-    @BeforeEach
     public void setup() {
         this.studentFileRepository = new StudentXMLRepo(
                 "fisiere/studentiTest.xml");
@@ -51,14 +53,13 @@ public class ServiceTest {
                 null
         );
     }
-
-    @AfterAll
     static void removeXML() {
         new File("fisiere/studentiTest.xml").delete();
     }
 
     @Test
     public void testAddStudentDuplicate() {
+        setup();
         Student newStudent1 = new Student("1", "Alex", 934, "alex@gmail.com");
 
         Student stud1 = this.service.addStudent(newStudent1);
@@ -68,10 +69,12 @@ public class ServiceTest {
         assertEquals(newStudent1.getID(), stud2.getID());
 
         this.service.deleteStudent("1");
+        removeXML();
     }
 
     @Test
     public void testAddStudentNonDuplicate(){
+        setup();
         Student newStudent1 = new Student("1", "Alex", 934, "alex@gmail.com");
         Student newStudent2 = new Student("2", "Alex", 934, "alex@gmail.com");
 
@@ -88,34 +91,41 @@ public class ServiceTest {
 
         this.service.deleteStudent("1");
         this.service.deleteStudent("2");
+        removeXML();
     }
 
     @Test
     public void testAddStudentValidName(){
+        setup();
         Student newStudent1 = new Student("1", "Alex", 934, "alex@gmail.com");
         this.service.addStudent(newStudent1);
         var students = this.service.getAllStudenti().iterator();
         assertEquals(students.next().getID(), newStudent1.getID());
         this.service.deleteStudent("1");
+        removeXML();
     }
 
     @Test
     public void testAddStudentEmptyName(){
+        setup();
         Student newStudent2 = new Student("2", "", 934, "alex@gmail.com");
         assertThrows(ValidationException.class, () -> this.service.addStudent(newStudent2));
-
+    removeXML();
     }
 
     @Test
     public void testAddStudentNullName(){
+        setup();
         Student newStudent3 = new Student("3", null, 934, "alex@gmail.com");
         assertThrows(ValidationException.class, () -> this.service.addStudent(newStudent3));
+        removeXML();
     }
 
 
 
     @Test
     public void testAddStudentValidGroup() {
+        setup();
         Student newStudent1 = new Student("1", "Alex", 934, "alex@gmail.com");
 
         this.service.addStudent(newStudent1);
@@ -123,54 +133,69 @@ public class ServiceTest {
         assertEquals(students.next().getID(), newStudent1.getID());
 
         this.service.deleteStudent("1");
+        removeXML();
     }
 
     @Test
     public void testAddStudentInvalidGroup() {
+        setup();
         Student newStudent2 = new Student("2", "Alex", -6, "alex@gmail.com");
         assertThrows(ValidationException.class, () -> this.service.addStudent(newStudent2));
+        removeXML();
     }
 
     @Test
     public void testAddStudentValidEmail() {
+        setup();
         Student newStudent1 = new Student("1", "Alex", 934, "alex@gmail.com");
         this.service.addStudent(newStudent1);
         var students = this.service.getAllStudenti().iterator();
         assertEquals(students.next().getID(), newStudent1.getID());
         this.service.deleteStudent("1");
+        removeXML();
     }
 
     @Test
     public void testAddStudentEmptyEmail() {
+        setup();
         Student newStudent2 = new Student("2", "Alex", 934, "");
         assertThrows(ValidationException.class, () -> this.service.addStudent(newStudent2));
+        removeXML();
     }
 
     @Test
     public void testAddStudentNullEmail() {
+        setup();
         Student newStudent3 = new Student("3", "Alex", 934, null);
         assertThrows(ValidationException.class, () -> this.service.addStudent(newStudent3));
+        removeXML();
     }
 
     @Test
     public void testAddStudentValidId() {
+        setup();
         Student newStudent1 = new Student("2345", "Alex", 934, "alex@gmail.com");
         this.service.addStudent(newStudent1);
         var students = this.service.getAllStudenti().iterator();
         assertEquals(students.next().getID(), newStudent1.getID());
         this.service.deleteStudent("2345");
+        removeXML();
     }
 
     @Test
     public void testAddStudentEmptyId() {
+        setup();
         Student newStudent2 = new Student("", "Alex", 934, "alex@gmail.com");
         assertThrows(ValidationException.class, () -> this.service.addStudent(newStudent2));
+        removeXML();
     }
 
     @Test
     public void testAddStudentNullId() {
+        setup();
         Student newStudent3 = new Student(null, "Alex", 934, "alex@gmail.com");
         assertThrows(ValidationException.class, () -> this.service.addStudent(newStudent3));
+        removeXML();
     }
 
     /**
@@ -178,10 +203,12 @@ public class ServiceTest {
      */
     @Test
     public void testAddStudentGroupLowerBVABound(){
+        setup();
         Student newStudent1 = new Student("1", "Alex", 0, "alex@gmail.com");
         this.service.addStudent(newStudent1);
         var students = this.service.getAllStudenti().iterator();
         assertEquals(students.next().getID(), newStudent1.getID());
         this.service.deleteStudent("1");
+        removeXML();
     }
 }
